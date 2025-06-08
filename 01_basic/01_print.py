c4c4adcf4c08bf124605d1c24afc0b6469661850
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2025-06-08 02:00:30 -0400

    the first step with python

---
+++ 01_basic/01_print.py
@@ -0,0 +1,7 @@
+# Hola Mundo
+
+print("Hola Mundo")
+print('Realizando con comillas simple')
+print('Python','es','genial')
+print('Python','con otro tipo', 'de separador', sep='-')
+print('Python','con otro tipo', 'de separador', end='!')
